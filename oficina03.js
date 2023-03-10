9c91347d4dd8b220027341b0f62a2207e744fb76
nicolas ferreira silva <126110122+nicolasferreira007@users.noreply.github.com>
2023-03-10 15:15:29 -0300

    Add files via upload

---
+++ oficina03.js
@@ -0,0 +1,16 @@
+let temDinheiro = true;
+let temSol = false;
+
+if (temDinheiro) {
+  if (temsol) {
+    console.log("guajueja");
+  } else {
+    console.log(restaurante);
+  }
+ } else {
+    if(temSol) {
+      console.log("ibirapuera");
+      } else {
+        console.log("aula");
+      }
+    }
---
+++ oficina04.js
@@ -0,0 +1,8 @@
+if(true){
+    let a =10;
+    console.log(a);
+
+}
+
+//console.log(a) fora do escopo
+//diferenca entre let e var(diferenca de escopo)
